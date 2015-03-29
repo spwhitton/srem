d3763e664f385059940ec6da1fb9f823a7cf6048
Sean Whitton <spwhitton@spwhitton.name>
2015-03-29 17:49:38 +0900

    config function to get cache directory location

--- src/Control/SremConfig.hs
+++ src/Control/SremConfig.hs
@@ -1,4 +1,13 @@
-module Control.SremConfig where
+module Control.SremConfig ( getCacheDirectory
+                          , intervals
+                          ) where
+
+import System.Directory (getHomeDirectory)
+import System.FilePath ((</>))
+
+getCacheDirectory :: IO FilePath
+getCacheDirectory = getHomeDirectory >>= \h ->
+    return $ h </> ".cache" </> "srem"
 
 intervals :: [Int]
 intervals = [60, 15, 0]
