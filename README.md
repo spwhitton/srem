51de5b2552f22c4099d8161c7e366716cbd43cc8
Sean Whitton <spwhitton@spwhitton.name>
2015-03-30 19:54:07 +0900

    GPL header in source files & skeleton README

---
+++ README.md
@@ -0,0 +1,17 @@
+# srem
+
+## Introduction
+
+TODO
+
+## Usage
+
+TODO
+
+## Building
+
+TODO
+
+## Installation
+
+TODO
--- src/Data/Maybe/Read.hs
+++ src/Data/Maybe/Read.hs
@@ -1,6 +1,6 @@
 module Data.Maybe.Read (readMaybe) where
 
--- readMaybe function from "Learn You a Haskell"
+-- readMaybe function from "Learn You a Haskell".  Used here under CC BY-NC-SA 3.0.
 
 readMaybe    :: (Read a) => String -> Maybe a
 readMaybe st = case reads st of
